b10d4d38fdf77959a2345acb271ca3267fc7fb0d
Spencer Link <spencrlink@gmail.com>
2024-01-10 01:23:16 -0600

    First commit

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -8,7 +8,23 @@ export const createPrediction = async (req, res) => {
       question: message,
     };
 
-    res.status(200).json({ message: "Demo Response" });
+    //call the flowise endpoint
+    const response = await fetch(
+      `${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization : `Bearer ${process.env.FLOWISE_API_KEY}` 
+        },
+        body: JSON.stringify(flowiseData),
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+
+    res.status(200).json({ message: data.text });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
